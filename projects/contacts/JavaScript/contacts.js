e60caefcab512623c9c4e07ec888edc1b56797cd
ARIEL MAZOR <ariel.mazor.mail.com>
2020-07-12 18:25:11 +0300

    ...

--- projects/contacts/JavaScript/contacts.js
+++ projects/contacts/JavaScript/contacts.js
@@ -1,46 +1,6 @@
 var Phonenumber = $("#phonenumber").val();
 var currIndex;
-var userlist = [
-  {
-    src:
-      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png",
-    fullName: "Yafa mazor",
-    address: "yocneam elite",
-    gender: "male",
-    phoneNumber: "2131314213",
-    delete: "../Images/delete.png",
-    edit: "../Images/edit.png",
-  },
-  {
-    src:
-      "https://s.yimg.com/ny/api/res/1.2/tKkCUB5xgXhjeL1CzWIfKQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/people_218/fa7c9bfd3d17b7170efd15f840684e0f",
-    fullName: "Bh",
-    address: "cfar saba",
-    gender: "fimale",
-    phoneNumber: "2131314213",
-    delete: "../Images/delete.png",
-    edit: "../Images/edit.png",
-  },
-  {
-    src:
-      "https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png",
-    fullName: "mm",
-    address: "criat motzkin",
-    gender: "male",
-    phoneNumber: 2131314213,
-    delete: "../Images/delete.png",
-    edit: "../Images/edit.png",
-  },
-  {
-    src:
-      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png",
-    fullName: "Rm",
-    address: "harzelia",
-    gender: "fimale",
-    phoneNumber: "2131314213",
-    delete: "../Images/delete.png",
-    edit: "../Images/edit.png",
-  },
+var userlist = [{}
 ];
 
 // ------------------------------------------
@@ -59,6 +19,7 @@ function build() {
 
 var obj;
 function onSave() {
+
   let obj =
     currIndex == -1
       ? { delete: "../Images/delete.png", edit: "../Images/edit.png",src:"../Images/avatar.png" }
@@ -71,6 +32,7 @@ function onSave() {
   if (currIndex == -1) {
     userlist.push(obj);
   }
+  $(".row-wrapper").addClass("show2")
 
   closeModal();
   build();
@@ -121,6 +83,7 @@ function showNewModal(index) {
 
   $(".modal .content .body").html(template(newObj));
   $(".modal").addClass("show1");
+  $(".xwrapper").addClass("show3");
 }
 
 // ------------------------------------------
@@ -133,5 +96,3 @@ function closeModal() {
 // ------------------------------------------
 // calls
 // ------------------------------------------
-
-build();
