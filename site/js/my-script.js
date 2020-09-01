14c2526a46f0d97dad8304abd88441ece0850d7e
arielmazor <ariel.mazor.mail@Gmail.com>
2020-09-01 11:08:34 +0300

    ...

--- site/js/my-script.js
+++ site/js/my-script.js
@@ -38,19 +38,24 @@ function href(num) {
 	}
 
 }
-function initProjectsCarousel() {
 
-	$("#slider").slick({
-		speed: 1000,
-		dots: true,
-		dotsClass: 'slide-dots',
-		prevArrow: '<div class="slick-prev "></div>',
-		nextArrow: '<div  class="slick-next "></div>',
-	});
+function initProjectsCarousel() {
+	$('.carousel-main').owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: false,
+		autoplayTimeout: 1500,
+		margin: 10,
+		nav: true,
+		dots: false,
+		navText: ['<span class="fas fa-chevron-left fa-2x"></span>', '<span class="fas fa-chevron-right fa-2x"></span>'],
+	})
 }
+
 
 
 $(document).ready(function () {
 	initHeader();
 	initProjectsCarousel();
 });
+
