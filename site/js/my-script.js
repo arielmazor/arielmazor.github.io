e0ecdf21c83aeb1b37b78b6a00d6fbf8d03db94f
arielmazor <ariel.mazor.mail@Gmail.com>
2020-09-02 13:27:30 +0300

    ...

--- site/js/my-script.js
+++ site/js/my-script.js
@@ -48,7 +48,7 @@ function initProjectsCarousel() {
 		margin: 10,
 		nav: true,
 		dots: false,
-		navText: ['<span class="fas fa-chevron-left fa-2x"></span>', '<span class="fas fa-chevron-right fa-2x"></span>'],
+		navText: ['<span class="wide-arrow left"></span>', '<span class="wide-arrow right"></span>'],
 	})
 }
 
