879dc6a4819f9a65d9a40957d67d4c04a6558b54
arielmazor <ariel.mazor.mail@Gmail.com>
2020-09-08 12:23:44 +0300

    ...

--- projects/contact/JavaScript/contact.js
+++ projects/contact/JavaScript/contact.js
@@ -2,13 +2,31 @@ function buildModal() {
     $(".modal").removeClass("hide")
     $(".modal").addClass("show")
 
-    var template = document.querySelector('#template').innerHTML;
-    var compiled_template = Handlebars.compile(template);
-    var rendered = compiled_template({ name: "Luke" });
-    document.querySelector('.modal-wrapper').innerHTML = rendered;
-}
 
-function closeModal() {
-    $(".modal").removeClass("show")
-    $(".modal").addClass("hide")
 }
+function saveData() {
+    var data = [];
+    data += $(".data-input").val()
+
+    if (data == undefined || data == "") {
+        $(".save-buton").addClass("disable")
+    } else {
+        $(".save-buton").removeClass("disable")
+    }
+}
+function imageSelect() {
+    $(".image-select-wrapper").addClass("image-select-show")
+}
+function closeModal(num) {
+    if (num == 1) {
+        $(".modal").removeClass("show")
+        $(".modal").addClass("hide")
+    } else {
+        $(".image-select-wrapper").removeClass("image-select-show")
+    }
+    num = 0;
+}
+// var template = document.querySelector('#template').innerHTML;
+// var compiled_template = Handlebars.compile(template);
+// var rendered = compiled_template({ name: "Luke" });
+// document.querySelector('.modal-wrapper').innerHTML = rendered
