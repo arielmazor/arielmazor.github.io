de86c0a1af136d4e1053f8ea7a5a01e513b7008e
arielmazor <ariel.mazor.mail@gmail.com>
2020-10-06 18:31:04 +0300

    ///

--- studio972/site/js/my-script.js
+++ studio972/site/js/my-script.js
@@ -1,7 +1,42 @@
-function showModal() {
+var arry = [];
+
+function showModal(filter, id) {
+  if ($(".cd-filters .filter a").hasClass("selected")) {
+    if (filter == 1) {
+      $(".modal").addClass("design");
+      $(".modal").removeClass("apps");
+
+    } else if (filter == 2) {
+      $(".modal").addClass("apps");
+      $(".modal").removeClass("design");
+    }
+  } else {
+    $(".modal").addClass("show");
+    $(".modal").removeClass("apps");
+    $(".modal").removeClass("design");
+    $(".modal .wrapper .image-wrapper").addClass("all");
+  }
   $(".modal").addClass("show");
   $("body").addClass("no-scroll");
+
+  setTimeout(function() {
+    $('.modal').animate({
+      scrollTop: $(`#${id}`).position().top
+    }, 500);
+  }, 50);
+
 }
+$('.btn-p').click(function() {
+  $('html, body').animate({
+    scrollTop: $('.protfolio').position().top
+  }, 700);
+})
+$('.btn-a ').click(function() {
+  $('html, body').animate({
+    scrollTop: $('.about').position().top
+  }, 700);
+})
+
 
 function closeModal() {
   $(".modal").removeClass("show");
@@ -10,7 +45,7 @@ function closeModal() {
 
 $('.protfilio-btn ').click(function() {
   $('html, body').animate({
-    scrollTop: $('.protfolio').position(200).top
+    scrollTop: $('.protfolio').position().top
   }, 700);
 })
 
