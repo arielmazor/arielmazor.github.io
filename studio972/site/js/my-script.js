b137e34a6d98c9705fe7ce17b20c378f56ada2a9
arielmazor <ariel.mazor.mail@gmail.com>
2020-10-08 11:59:51 +0300

    ...

--- studio972/site/js/my-script.js
+++ studio972/site/js/my-script.js
@@ -1,69 +1,74 @@
-var arry = [];
 
-function showModal(filter, id) {
+var arry = [];
+var filter = -1;
+
+/* -----------------------------------------------
+get filter for gallery
+----------------------------------------------- */
+function setFilter(num){
+filter = num;
+}
+
+/* -----------------------------------------------
+show modal
+----------------------------------------------- */
+function showModal( id) {
   var height = 0;
   var _arry;
-
+/* -----------------------------------------------*/
   if (filter == 1) {
-    $(".modal").removeClass("apps").addClass("design");
+    $(".modal").removeClass("apps").removeClass("all").addClass("design");
     _arry = $(".modal .image-wrapper.design");
   } else if (filter == 2) {
-    $(".modal").removeClass("design").addClass("apps");
-    _arry = $(".modal .image-wrapper.design");
+    $(".modal").removeClass("design").removeClass("all").addClass("apps");
+    _arry = $(".modal .image-wrapper.apps");
+  }else {
+    $(".modal").removeClass("design").removeClass("apps").addClass("all")
+    _arry = $(".modal .image-wrapper");
   }
-
+/* -----------------------------------------------*/
   for (var i = 0; i < _arry.length; i++) {
     if (_arry[i].id == id) {
       break;
     } else {
       height += $(`#${_arry[i].id}`).height();
-      console.log(height);
-
     }
   }
-  console.log(height);
-  // } else {
-  //   $(".modal").addClass("show");
-  //   $(".modal").removeClass("apps");
-  //   $(".modal").removeClass("design");
-  //   $(".modal .wrapper .image-wrapper").addClass("all");
-  // }
+/* -----------------------------------------------*/
   $(".modal").addClass("show");
   $("body").addClass("no-scroll");
-
-
-
-  setTimeout(function() {
-    $('.modal').animate({
-      scrollTop: height
-    }, 500);
-  }, 50);
-
-
+  /* -----------------------------------------------*/
+    $('.modal').scrollTop(height);
 }
+/* -----------------------------------------------
+anintion top
+----------------------------------------------- */
 $('.btn-p').click(function() {
   $('html, body').animate({
     scrollTop: $('.protfolio').position().top
   }, 700);
 })
+  /* -----------------------------------------------*/
 $('.btn-a ').click(function() {
   $('html, body').animate({
     scrollTop: $('.about').position().top
   }, 700);
 })
-
-
-function closeModal() {
-  $(".modal").removeClass("show");
-  $("body").removeClass("no-scroll");
-}
-
 $('.protfilio-btn ').click(function() {
   $('html, body').animate({
     scrollTop: $('.protfolio').position().top
   }, 700);
 })
-
+/* -----------------------------------------------
+cose modal
+----------------------------------------------- */
+function closeModal() {
+  $(".modal").removeClass("show");
+  $("body").removeClass("no-scroll");
+}
+/* -----------------------------------------------
+header change color 
+----------------------------------------------- */
 jQuery(document).ready(function() {
   $(window).scroll(function() {
     if ($('body').scrollTop() > 20) {
