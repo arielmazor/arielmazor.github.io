a6584f71ee889c806b0527b60d7fdbb5d5705d1d
arielmazor <ariel.mazor.mail@gmail.com>
2020-10-22 12:16:02 +0300

    ...

--- ocean-templates/Data conversion/js/my-script.js
+++ ocean-templates/Data conversion/js/my-script.js
@@ -1,16 +1,23 @@
 /* -----------------------------------------------
 nextStep
 ----------------------------------------------- */
-function nextStep(stepNum){
+function nextStep(stepNum) {
   var _id = `step${stepNum}`
-$(".step .step-title").removeClass("active");
-$(".step .wrapper").removeClass("active");
-$(`#${_id} .step-title`).addClass("active");
-$(`#${_id} .wrapper`).addClass("active");
-console.log(_id)
+  $(".step .step-title").removeClass("active");
+  $(".step .wrapper").removeClass("active");
+  $(`#${_id} .step-title`).addClass("active");
+  $(`#${_id} .wrapper`).addClass("active");
+  console.log(_id)
 
 }
-
+/* -----------------------------------------------
+exampleActive
+----------------------------------------------- */
+$('.ask-icon-wrapper ').click(function() {
+  $('.width ,.input2').animate({ height: '72px' }, 500);
+  $(".width").focus();
+  document.getElementById("input2").blur();
+})
 jQuery(document).ready(function() {
   nextStep(1);
 })
