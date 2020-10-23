aa65b53a23797f284f5a87904868d8e210875c86
arielmazor <ariel.mazor.mail@gmail.com>
2020-10-23 14:14:08 +0300

    ...

--- ocean-templates/Data conversion/js/my-script.js
+++ ocean-templates/Data conversion/js/my-script.js
@@ -10,24 +10,10 @@ function nextStep(stepNum) {
   console.log(_id)
 
 }
-/* -----------------------------------------------
-exampleActive
------------------------------------------------ */
-$('.ask-icon-wrapper ').click(function() {
-  $('.width ,.input2').animate({
-    height: '92px'
-  }, 500);
-  $(".width").focus();
-  document.getElementById("input2").blur();
-  $(".none").css("display", "none")
-  $(".width").css("pointer-events", "none")
-  setTimeout(function() {
-    $(".input-wrapper .help-wrapper").css("opacity", "1")
-    $(".input-wrapper .help-wrapper").css("width", "100%")
-  }, 200);
 
 
-})
+
+
 jQuery(document).ready(function() {
   nextStep(1);
 })
