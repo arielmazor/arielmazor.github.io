afe9c4908625a472704acbd2379732313fea7a2a
arielmazor <ariel.mazor.mail@gmail.com>
2020-10-28 17:50:55 +0200

    ץץץץ

--- ocean-templates/js/my-script.js
+++ ocean-templates/js/my-script.js
@@ -1,6 +1,9 @@
-var _templateData = {
+var templateData = {
   name: "ariel1",
   title: "ariel2",
+  discordLink: null,
+  instagramLink: null,
+  behanceLink: null,
 };
 
 /* -----------------------------------------------
@@ -17,22 +20,48 @@ function nextStep(stepNum) {
 }
 
 function showPreview() {
-  _templateData.name = $("#input1").val();
-  _templateData.title = $("#input2").val();
+  templateData.name = $("#input1").val();
+  templateData.title = $("#input2").val();
+  templateData.discordLink = $("#discord-link-input").val();
+  templateData.instagramLink = $("#instagram-link-input").val();
+  templateData.behanceLink = $("#behance-link-input").val();
+
+
 
   var compiled_template = Handlebars.compile(templateStr);
-  var html = compiled_template(_templateData);
-
+  var html = compiled_template(templateData);
   var ifrm = document.querySelector("#template-iframe")
   let doc = ifrm.contentDocument ? ifrm.contentDocument : ifrm.contentWindow.document;
 
   doc.open();
   doc.write(html);
   doc.close();
+
+
   $(".iframe-wrapper").addClass("show")
   $("body").css("overflow", "hidden")
   $("#template-iframe").css("border", "none")
   $(".iframe-body ").css("background", "#E7E7E7")
+    // setTimeout(function() {
+    //   if (templateData.discordLink == "") {
+    //     console.log(templateData.discordLink == "")
+    //     $(".discord-icon").addClass("close");
+    //   } else {
+    //     $(".discord-icon").removeClass("close");
+    //   }
+    //   if (templateData.instagramLink == "") {
+    //     $(".instagram-icon").addClass("close");
+    //   } else {
+    //     $(".instagram-icon").removeClass("close");
+    //   }
+    //   if (templateData.behanceLink == "") {
+    //     $(".behance-icon").addClass("close");
+    //   } else {
+    //     $(".behance-icon").removeClass("close");
+    //   }
+    //   $(".discord-icon").addClass("close");
+
+  // }, 1000);
 }
 
 
