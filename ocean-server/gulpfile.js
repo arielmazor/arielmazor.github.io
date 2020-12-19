c9e5e6f9f9c7eaf8f76f8498d5367224881b9646
arielmazor <ariel.mazor.mail@gmail.com>
2020-12-19 22:42:17 +0200

    ...

--- ocean-server/gulpfile.js
+++ ocean-server/gulpfile.js
@@ -15,10 +15,25 @@ function style() {
     .pipe(sass())
     .pipe(gulp.dest('../tempni/production/site/css'))
     .pipe(browserSync.stream());
+
+}
+
+function html() {
+  return gulp.src('../tempni/dev/site/**/*.html')
+    .pipe(sass())
+    .pipe(gulp.dest('../tempni/production/site'))
+    .pipe(browserSync.stream());
+}
+
+function js() {
+  return gulp.src('../tempni/dev/site/**/*.js')
+    .pipe(sass())
+    .pipe(gulp.dest('../tempni/production/site/js'))
+    .pipe(browserSync.stream());
 }
 
 function watch() {
   gulp.watch('../tempni/dev/site/**/*.scss', style);
 }
-exports.style = style;
+exports.all = style, html, js;
 exports.watch = watch;
