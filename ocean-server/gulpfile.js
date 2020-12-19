1e29cbf145aa067386234dcecbcf2d9f871df021
arielmazor <ariel.mazor.mail@gmail.com>
2020-12-19 22:52:11 +0200

    ...

--- ocean-server/gulpfile.js
+++ ocean-server/gulpfile.js
@@ -4,36 +4,47 @@
 
 
 const browserSync = require('browser-sync').create();
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+import {
+  src,
+  dest,
+  watch as __watch
+} from 'gulp';
+import sass, {
+  compiler
+} from 'gulp-sass';
 
-sass.compiler = require('node-sass');
+compiler = require('node-sass');
 
 
-function style() {
-  return gulp.src('../tempni/dev/site/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('../tempni/production/site/css'))
-    .pipe(browserSync.stream());
+function _export() {
+
+  function style() {
+    return src('../tempni/dev/site/**/*.scss')
+      .pipe(sass())
+      .pipe(dest('../tempni/production/site/css'))
+      .pipe(browserSync.stream());
+  }
+
+  function html() {
+    return src('../tempni/dev/site/*.html')
+      .pipe(sass())
+      .pipe(dest('../tempni/production/site'))
+      .pipe(browserSync.stream());
+  }
+
+  function js() {
+    return src('../tempni/dev/site/**/*.js')
+      .pipe(sass())
+      .pipe(dest('../tempni/production/site/js'))
+      .pipe(browserSync.stream());
+  }
+
+  function watch() {
+    __watch('../tempni/dev/site/**/*.scss', style);
+    __watch('../tempni/dev/site/**/*.js', js);
+    __watch('../tempni/dev/site/*.html', html);
+  }
 
 }
-
-function html() {
-  return gulp.src('../tempni/dev/site/**/*.html')
-    .pipe(sass())
-    .pipe(gulp.dest('../tempni/production/site'))
-    .pipe(browserSync.stream());
-}
-
-function js() {
-  return gulp.src('../tempni/dev/site/**/*.js')
-    .pipe(sass())
-    .pipe(gulp.dest('../tempni/production/site/js'))
-    .pipe(browserSync.stream());
-}
-
-function watch() {
-  gulp.watch('../tempni/dev/site/**/*.scss', style);
-}
-exports.all = style, html, js;
-exports.watch = watch;
+exports.all = _export;
+export const _all = _export;
