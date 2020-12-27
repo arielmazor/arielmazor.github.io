dd71d9976332cfa8b369942a3a46d2009e73806b
arielmazor <ariel.mazor.mail@gmail.com>
2020-12-27 18:48:06 +0200

    ץץץ

--- tempni/prod/site/js/main.js
+++ tempni/prod/site/js/main.js
@@ -1,26 +1,51 @@
-const videosArr = [2020, 2020, 2020]
+const videosArr = [7020, 7020, 7020]
+var playTimeOut;
+var inAction = false;
 
 function playVideo(i) {
-  console.log(i);
   if (i == videosArr.length) {
     i = 0;
   }
 
-  // $('.vid').each(function() {
-  //   $(this).get(0).pause();
-  //   $(this).get(0).currentTime = 0;
-  // });
+  $('.vid').each(function () {
+    $(this).get(0).pause();
+    $(this).get(0).currentTime = 0;
+  });
 
   $(".video-interaction-wrap").removeClass("selected");
   $(`.video-interaction-wrap.i-${i}`).addClass('selected');
   $(`.video-wrapper`).removeClass('show');
   $(`.video-wrapper.wrap-${i}`).addClass('show');
-  // $(`.video-wrapper.wrap-${i} .vid${i}`).play();
+  $(`.video-wrapper.wrap-${i} .vid`).get(0).play();
 
 
-  setTimeout(function() {
-    playVideo(i + 1);    
+  playTimeOut = setTimeout(function () {
+    playVideo(i + 1);
   }, videosArr[i]);
 }
 
-playVideo(0)
+function onInteractionClick(i) {
+  runPlayVideo(i);
+}
+
+function runPlayVideo(i) {
+  if (playTimeOut) {
+    clearTimeout(playTimeOut);
+  }
+  playVideo(i);
+}
+
+$(document).on('scroll', function () {
+  console.log($(window).scrollTop())
+  if ($(window).scrollTop() >= 700) {
+    runPlayVideo(0);
+  }
+})
+
+$(document).ready(function () {
+  if (playTimeOut)
+
+    if ($(window).scrollTop() >= 700) {
+      runPlayVideo(0)
+    }
+});
