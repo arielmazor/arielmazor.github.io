de2c6fb2e247dfb406a0f0b1f07750c2c99836cc
arielmazor <ariel.mazor.mail@gmail.com>
2021-01-06 18:07:07 +0200

    ...

--- ocean-server/gulpfile.js
+++ ocean-server/gulpfile.js
@@ -33,23 +33,15 @@
       gulp.src(['../tempni/dev/**/*.*', '!../tempni/dev/**/*.scss'])
         .pipe(gulp.dest('../tempni/prod'))
         .on('end', () => {
-          // console.log("22222222222")
           cb();
         })
     }, 500);
   };
 
   function watch(cb) {
-    gulp.watch('../tempni/dev/site/**/*.scss', style)
-    //------------------------------------------------------------
-    gulp.watch('../tempni/dev/**/*.html').on('change', () => {
-      clean(cb);
-      copyFiles(cb);
-    });
-    gulp.watch('../tempni/dev/**/*.js').on('change', () => {
-      clean(cb);
-      copyFiles(cb);
-    });
+    gulp.watch('../tempni/dev/site/**/*.scss', style);
+    gulp.watch('../tempni/dev/**/*.html', gulp.series(clean, copyFiles, style));
+    gulp.watch('../tempni/dev/**/*.js', gulp.series(clean, copyFiles, style));
   }
 
   exports.default = gulp.task('default', gulp.series(clean, copyFiles, style));
