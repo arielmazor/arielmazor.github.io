9394ba8d99968e262a35ce702ca9669968542839
arielmazor <ariel.mazor.mail@gmail.com>
2021-01-08 13:16:52 +0200

    ...

--- ocean-server/gulpfile.js
+++ ocean-server/gulpfile.js
@@ -1,49 +1,50 @@
-  //------------------------------------
-  // sass
-  //------------------------------------
+//------------------------------------
+// sass
+//------------------------------------
 
-  const gulp = require('gulp');
-  const sass = require('gulp-sass');
-  const del = require('del');
-  const {} = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const { } = require('gulp');
 
-  function clean(cb) {
-    del(['../tempni/prod'], {
-      force: true
+function clean(cb) {
+  del(['../tempni/prod'], {
+    force: true
+  });
+  console.log("1111111")
+  cb();
+}
+
+function style(cb) {
+
+
+  gulp.src('../tempni/dev/site/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('../tempni/prod/site'))
+    .on('error', cb)
+    .on('end', () => {
+      // console.log("3333333333")
+      cb();
     });
-    console.log("1111111")
-    cb();
-  }
+}
 
-  function style(cb) {
-
-
-    gulp.src('../tempni/dev/site/**/*.scss')
-      .pipe(sass())
-      .pipe(gulp.dest('../tempni/prod/site'))
-      .on('error', cb)
+function copyFiles(cb) {
+  setTimeout(() => {
+    gulp.src(['../tempni/dev/**/*.*', '!../tempni/dev/**/*.scss'])
+      .pipe(gulp.dest('../tempni/prod'))
       .on('end', () => {
-        // console.log("3333333333")
         cb();
-      });
-  }
+      })
+  }, 500);
+};
 
-  function copyFiles(cb) {
-    setTimeout(() => {
-      gulp.src(['../tempni/dev/**/*.*', '!../tempni/dev/**/*.scss'])
-        .pipe(gulp.dest('../tempni/prod'))
-        .on('end', () => {
-          cb();
-        })
-    }, 500);
-  };
+function watch(cb) {
+  gulp.watch('../tempni/dev/site/**/*.scss', style);
+  gulp.watch('../tempni/dev/**/*.html', gulp.series(clean, copyFiles, style));
+  gulp.watch('../tempni/dev/**/*.js', gulp.series(clean, copyFiles, style));
+  gulp.watch('../tempni/dev/site/img/*.**', gulp.series(clean, copyFiles, style));
+}
 
-  function watch(cb) {
-    gulp.watch('../tempni/dev/site/**/*.scss', style);
-    gulp.watch('../tempni/dev/**/*.html', gulp.series(clean, copyFiles, style));
-    gulp.watch('../tempni/dev/**/*.js', gulp.series(clean, copyFiles, style));
-  }
+exports.default = gulp.task('default', gulp.series(clean, copyFiles, style));
 
-  exports.default = gulp.task('default', gulp.series(clean, copyFiles, style));
-
-  exports.watch = watch;
+exports.watch = watch;
