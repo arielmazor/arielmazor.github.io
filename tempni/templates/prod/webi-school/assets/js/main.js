e6f8e4671ed8bec25fdfc0730d267e1fd77a2042
arielmazor <64742335+arielmazor@users.noreply.github.com>
2021-01-29 16:37:07 +0200

    ...

--- tempni/templates/prod/webi-school/assets/js/main.js
+++ tempni/templates/prod/webi-school/assets/js/main.js
@@ -1,8 +1,15 @@
 
 
-function suggest(num) {
+function suggest(num, item) {
+    $(`.sugg-wrap div`).removeClass('_active');
+    $(`.pf-wrap .i`).removeClass('_active');
+    $(`.sugg-wrap .i${num}`).addClass('_active');
+    $(`.pf-wrap .${item}`).addClass('_active');
     $("._star").removeClass("active");
     $(`.star-wrapper .star${num}`).addClass("active");
+}
+function star(item) {
+
 }
 
 $(document).on('scroll', function () {
@@ -13,6 +20,8 @@ $(document).on('scroll', function () {
 })
 
 $(document).ready(function () {
+    $(`.sugg-wrap .i1`).addClass('_active');
+    $(`.pf-wrap .i1`).addClass('_active');
     $(`.star-wrapper .star1`).addClass("active");
     if ($(window).scrollTop() >= $('.about-page').position().top - 1000) {
         $(".img-leng img").addClass("fade-in-up");
