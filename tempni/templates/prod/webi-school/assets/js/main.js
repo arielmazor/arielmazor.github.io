30d1a746fa0542e62e6763d700cd8ba7f542a544
arielmazor <64742335+arielmazor@users.noreply.github.com>
2021-02-05 16:13:14 +0200

    ...

--- tempni/templates/prod/webi-school/assets/js/main.js
+++ tempni/templates/prod/webi-school/assets/js/main.js
@@ -1,5 +1,3 @@
-
-
 function suggest(num, item) {
     $(`.sugg-wrap div`).removeClass('_active');
     $(`.pf-wrap .i`).removeClass('_active');
@@ -8,13 +6,33 @@ function suggest(num, item) {
     $("._star").removeClass("active");
     $(`.star-wrapper .star${num}`).addClass("active");
 }
-function star(item) {
 
+function openDes(i) {
+    // const isOpen = $(`#i${i}`).hasClass(`open`)
+    // if (isOpen) {
+    //     $(`.item`).removeClass("open")
+    // } else {
+    //     $(`.item`).removeClass("open")
+    //     $(`#i${i}`).addClass("open")
+    // }
+
+    $(".what-i-do .item").on("click", function () {
+        if ($(this).hasClass("open")) {
+            $(this).removeClass("open");
+            $(this).find(".content").slideUp(350);
+        } else {
+            $(".what-i-do .item").removeClass("open");
+            $("what-i-do .item .content").slideUp(350);
+            $(this).addClass("open");
+            $(this).find(".content").slideDown(350);
+        }
+    });
 }
+
 
 $(document).on('scroll', function () {
 
-    if ($(window).scrollTop() >= $('.about-page').position().top - 1000) {
+    if ($(window).scrollTop() >= $('.what-i-teach').position().top - 1000) {
         $(".img-leng img").addClass("fade-in-up");
     }
 })
@@ -23,7 +41,7 @@ $(document).ready(function () {
     $(`.sugg-wrap .i1`).addClass('_active');
     $(`.pf-wrap .i1`).addClass('_active');
     $(`.star-wrapper .star1`).addClass("active");
-    if ($(window).scrollTop() >= $('.about-page').position().top - 1000) {
+    if ($(window).scrollTop() >= $('.what-i-teach').position().top - 1000) {
         $(".img-leng img").addClass("fade-in-up");
     }
 });
