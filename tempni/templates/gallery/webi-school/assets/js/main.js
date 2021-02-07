95aaf2ee08d04bbe0926c2a8900e56da042160c0
arielmazor <64742335+arielmazor@users.noreply.github.com>
2021-02-07 10:25:48 +0200

    default

--- tempni/templates/gallery/webi-school/assets/js/main.js
+++ tempni/templates/gallery/webi-school/assets/js/main.js
@@ -1,3 +1,19 @@
+function initWhatIdoAccordion() {
+    $(".what-i-do .item").on("click", function () {
+        $('.what-i-do .item').find(".content").slideUp(350);
+
+        if ($(this).hasClass("open")) {
+            $(this).removeClass("open");
+            $(this).find(".content").slideUp(350);
+        } else {
+            $(".what-i-do .item").removeClass("open");
+            $(this).find(".content").slideDown(350);
+            $(this).addClass("open");
+            $(this).find(".content").slideDown(350);
+        }
+    });
+}
+
 function suggest(num, item) {
     $(`.sugg-wrap div`).removeClass('_active');
     $(`.pf-wrap .i`).removeClass('_active');
@@ -7,29 +23,6 @@ function suggest(num, item) {
     $(`.star-wrapper .star${num}`).addClass("active");
 }
 
-function openDes(i) {
-    // const isOpen = $(`#i${i}`).hasClass(`open`)
-    // if (isOpen) {
-    //     $(`.item`).removeClass("open")
-    // } else {
-    //     $(`.item`).removeClass("open")
-    //     $(`#i${i}`).addClass("open")
-    // }
-
-    $(".what-i-do .item").on("click", function () {
-        if ($(this).hasClass("open")) {
-            $(this).removeClass("open");
-            $(this).find(".content").slideUp(350);
-        } else {
-            $(".what-i-do .item").removeClass("open");
-            $("what-i-do .item .content").slideUp(350);
-            $(this).addClass("open");
-            $(this).find(".content").slideDown(350);
-        }
-    });
-}
-
-
 $(document).on('scroll', function () {
 
     if ($(window).scrollTop() >= $('.what-i-teach').position().top - 1000) {
@@ -38,6 +31,7 @@ $(document).on('scroll', function () {
 })
 
 $(document).ready(function () {
+    initWhatIdoAccordion();
     $(`.sugg-wrap .i1`).addClass('_active');
     $(`.pf-wrap .i1`).addClass('_active');
     $(`.star-wrapper .star1`).addClass("active");
