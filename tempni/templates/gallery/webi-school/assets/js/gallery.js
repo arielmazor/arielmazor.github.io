be9965a597e6b48d0ced48aff8a225cd27a14a1e
arielmazor <64742335+arielmazor@users.noreply.github.com>
2021-02-11 20:16:51 +0200

    ץץץ

---
+++ tempni/templates/gallery/webi-school/assets/js/gallery.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    singleItem: true,
+    itemsScaleUp: true,
+    slideSpeed: 500,
+    // autoPlay: 3500,
+    autoPlay: false,
+    stopOnHover: true,
+    dots: true,
+    mouseDrag: false,
+  });
+});
--- tempni/templates/prod/webi-school/assets/js/main.js
+++ tempni/templates/prod/webi-school/assets/js/main.js
@@ -2,16 +2,31 @@ function flip(i) {
     $(`.flip-front`).animate({
         opacity: 0,
     }, 100);
+    $(`#item${i}.flip-back`).animate({
+        opacity: 1,
+    }, 100);
     $(`#item${i}.flip-back`).css("transform", "rotateY(" + 360 + "deg)");
-}
+    $(`.flip-front`).css("transform", "rotateY(" + 360 + "deg)");
 
+}
+function flipBack(i) {
+    $(`#item${i}.flip-back`).animate({
+        opacity: 0,
+    }, 100);
+    $(`.flip-front`).animate({
+        opacity: 1,
+    }, 100);
+    $(`#item${i}.flip-back`).css("transform", "rotateY(" + 263 + "deg)");
+    $(`.flip-front`).css("transform", "rotateY(" + 360 + "deg)");
+
+}
 function suggest(num, item) {
-    $(`.sugg-wrap div`).removeClass('_active');
-    $(`.pf-wrap .i`).removeClass('_active');
-    $(`.sugg-wrap .i${num}`).addClass('_active');
-    $(`.pf-wrap .${item}`).addClass('_active');
+    $(`.sugg - wrap div`).removeClass('_active');
+    $(`.pf - wrap.i`).removeClass('_active');
+    $(`.sugg - wrap.i${num}`).addClass('_active');
+    $(`.pf - wrap.${item}`).addClass('_active');
     $("._star").removeClass("active");
-    $(`.star-wrapper .star${num}`).addClass("active");
+    $(`.star - wrapper.star${num}`).addClass("active");
 }
 
 // $(document).on('scroll', function () {
@@ -22,9 +37,9 @@ function suggest(num, item) {
 // })
 
 $(document).ready(function () {
-    $(`.sugg-wrap .i1`).addClass('_active');
-    $(`.pf-wrap .i1`).addClass('_active');
-    $(`.star-wrapper .star1`).addClass("active");
+    $(`.sugg - wrap.i1`).addClass('_active');
+    $(`.pf - wrap.i1`).addClass('_active');
+    $(`.star - wrapper.star1`).addClass("active");
     // if ($(window).scrollTop() >= $('.what-i-teach').position().top - 1000) {
     //     $(".img-leng img").addClass("fade-in-up");
     // }
