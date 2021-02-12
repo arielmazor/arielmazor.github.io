9ecf4e86b579d3d2be914d5d78b7feacab142e4b
arielmazor <64742335+arielmazor@users.noreply.github.com>
2021-02-12 15:19:42 +0200

    ץץץץ

--- tempni/templates/prod/webi-school/assets/js/gallery.js
+++ tempni/templates/prod/webi-school/assets/js/gallery.js
@@ -1,13 +1,27 @@
+var isTestimonialsInit = false;
+
+function initTestimonials() {
+  if (!isTestimonialsInit && $(window).scrollTop() >= 1910) {
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      singleItem: true,
+      itemsScaleUp: true,
+      slideSpeed: 500,
+      // autoPlay: 3500,
+      autoPlay: 5100,
+      stopOnHover: true,
+      dots: true,
+      mouseDrag: false,
+    });
+  }
+}
+
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    items: 1,
-    singleItem: true,
-    itemsScaleUp: true,
-    slideSpeed: 500,
-    // autoPlay: 3500,
-    autoPlay: false,
-    stopOnHover: true,
-    dots: true,
-    mouseDrag: false,
-  });
+  initTestimonials();
 });
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 1910) {
+    initTestimonials();
+  }
+})
